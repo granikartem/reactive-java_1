c39488a145b459b8446c46fa848cf8cb163114cd
a.granik <a.granik@just-ai.com>
2024-10-22 20:29:38 +0300

    Collector using stream API.

--- src/main/java/org/reactive_java/collector/TaskStatusStatsCollector.java
+++ src/main/java/org/reactive_java/collector/TaskStatusStatsCollector.java
@@ -2,7 +2,6 @@ package org.reactive_java.collector;
 
 import org.reactive_java.model.Evaluation;
 import org.reactive_java.model.Task;
-import org.reactive_java.model.TaskStatus;
 import org.reactive_java.model.User;
 
 import java.time.Duration;
@@ -30,14 +29,20 @@ public class TaskStatusStatsCollector implements Collector<Task, Map<User, Map<T
         return (map, task) -> {
             User user = task.getUser();
             Evaluation evaluation = task.getEvaluation();
-            boolean taskCompletedOnTime = true;
+            boolean taskCompletedOnTime = task.getStatuses()
+                    .stream()
+                    .noneMatch(
+                            taskStatus -> Duration.between(taskStatus.startTime(), taskStatus.finishTime())
+                            .compareTo(evaluation.getStatusDurationMap().get(taskStatus.status())) > 0
+            );
 
-            Iterator<TaskStatus> taskStatusIterator = task.getStatuses().iterator();
+//            Iterator<TaskStatus> taskStatusIterator = task.getStatuses().iterator();
+//
+//            while (taskStatusIterator.hasNext()) {
+//                TaskStatus taskStatus = taskStatusIterator.next();
+//                taskCompletedOnTime &= Duration.between(taskStatus.startTime(), taskStatus.finishTime()).compareTo(evaluation.getStatusDurationMap().get(taskStatus.status())) <= 0;
+//            }
 
-            while (taskStatusIterator.hasNext()) {
-                TaskStatus taskStatus = taskStatusIterator.next();
-                taskCompletedOnTime &= Duration.between(taskStatus.startTime(), taskStatus.finishTime()).compareTo(evaluation.getStatusDurationMap().get(taskStatus.status())) <= 0;
-            }
 
             if (map.containsKey(user)) {
                 map.get(user).put(task, taskCompletedOnTime);
@@ -50,16 +55,23 @@ public class TaskStatusStatsCollector implements Collector<Task, Map<User, Map<T
     @Override
     public BinaryOperator<Map<User, Map<Task, Boolean>>> combiner() {
         return (map1, map2) -> {
-            Iterator<Map.Entry<User, Map<Task, Boolean>>> map2Iterator = map2.entrySet().iterator();
-            while (map2Iterator.hasNext()) {
-                Map.Entry<User, Map<Task, Boolean>> entry = map2Iterator.next();
-                User user = entry.getKey();
+            map2.forEach((user, value) -> {
                 if (map1.containsKey(user)) {
-                    map1.get(user).putAll(entry.getValue());
-                } else{
-                    map1.put(user, entry.getValue());
+                    map1.get(user).putAll(value);
+                } else {
+                    map1.put(user, value);
                 }
-            }
+            });
+//            Iterator<Map.Entry<User, Map<Task, Boolean>>> map2Iterator = map2.entrySet().iterator();
+//            while (map2Iterator.hasNext()) {
+//                Map.Entry<User, Map<Task, Boolean>> entry = map2Iterator.next();
+//                User user = entry.getKey();
+//                if (map1.containsKey(user)) {
+//                    map1.get(user).putAll(entry.getValue());
+//                } else {
+//                    map1.put(user, entry.getValue());
+//                }
+//            }
             return map1;
         };
     }
